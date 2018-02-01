93417073280ad5f33b916e8a3917e0bb96fb9447
Hugh Morrison <hmorri32@gmail.com>
2018-02-01 16:04:39 -0700

    array wrapper

---
+++ lib/stuff.rb
@@ -0,0 +1,18 @@
+require_relative "custom_enumerable"
+
+class ArrayWrapper
+  include CustomEnumerable
+
+  def initialize(*items)
+    @items = items.flatten
+  end
+
+  def each(&block)
+    @items.each(&block)
+    self
+  end
+
+  def ==(other)
+    @items == other
+  end
+end
