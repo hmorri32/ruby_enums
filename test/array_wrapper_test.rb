96591b7743237074b0a2167ec74600a9751008a7
Hugh Morrison <hmorri32@gmail.com>
2018-02-03 18:05:36 -0700

    defiens setup method

--- test/array_wrapper_test.rb
+++ test/array_wrapper_test.rb
@@ -4,32 +4,33 @@ require 'pry'
 require_relative "../lib/stuff.rb"
 
 class ArrayWrapperTest < Minitest::Test
+  attr_reader :items
+
+  def setup
+    @items  = ArrayWrapper.new(1,2,3,4)
+  end
+
   def test_map
-    items  = ArrayWrapper.new(1,2,3,4)
     result = items.map { |n| n * 2}
     assert_equal result, [2, 4, 6, 8]
   end
 
   def test_find
-    items  = ArrayWrapper.new(1, 2, 3, 4)
     result = items.find { |element| element == 3 }
     assert_equal result, 3
   end
 
   def test_find_all
-    items  = ArrayWrapper.new(1, 2, 3, 4)
     result = items.find_all { |element| element > 2 }
     assert_equal result, [3, 4]
   end
 
   def test_find_all_with_no_result
-    items  = ArrayWrapper.new(1, 2, 3, 4)
     result = items.find_all { |element| element > 4 }
     assert_equal result, []
   end
 
   def test_reduce_sum
-    items  = ArrayWrapper.new(1, 2, 3, 4)
     result = items.reduce(0) { |acc, element| acc + element }
     assert_equal 10, result
   end
@@ -41,19 +42,16 @@ class ArrayWrapperTest < Minitest::Test
   end
 
   def test_reduce_with_operation
-    items  = ArrayWrapper.new(1, 2, 3, 4)
     result = items.reduce(0, :+)
     assert_equal 10, result
   end
 
   def test_reduce_no_acc
-    items  = ArrayWrapper.new(1, 2, 3, 4)
     result = items.reduce(:+)
     assert_equal 10, result
   end
 
   def test_first
-    items = ArrayWrapper.new(1, 2, 3, 4)
     assert_equal items.first, 1
   end
 
